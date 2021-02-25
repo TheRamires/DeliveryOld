4546d2b26ae7683285b37cadad3ebfada3b54bb2
exercise <theendorfin@mail.ru>
2021-02-25 22:18:32 +0300

    #

--- app/src/main/java/com/example/delivery/menu/MenuRepositoriy.java
+++ app/src/main/java/com/example/delivery/menu/MenuRepositoriy.java
@@ -8,14 +8,19 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.delivery.data.Entity;
 import com.example.delivery.data.TestData;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MenuRepositoriy {
     private TestData testData=new TestData();
 
     public void requestData(MutableLiveData<List<Entity>> listLive){
-        //test
-        listLive.setValue(testData.data());
+        List<Entity> list=testData.data();
+        Collections.sort(list, (Entity o1, Entity o2)-> {
+                return o1.getBrand().compareTo(o2.getBrand());
+        });
+        listLive.setValue(list);
     }
     public void requestParams1(MutableLiveData<List<String>> section1Live){
         section1Live.setValue(testData.section1Drawables());
